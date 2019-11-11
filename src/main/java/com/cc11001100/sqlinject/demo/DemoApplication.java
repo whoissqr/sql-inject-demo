abff9102f105f5c32fa6eace622830c5e0111e18
siguser <chaoshi@synopsys.com>
2019-11-11 16:47:19 +0800

    fake a insecure cookie defect.

--- src/main/java/com/cc11001100/sqlinject/demo/DemoApplication.java
+++ src/main/java/com/cc11001100/sqlinject/demo/DemoApplication.java
@@ -2,11 +2,21 @@ package com.cc11001100.sqlinject.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Cookie;
 
 @SpringBootApplication
+
+
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	void addSessionToken(HttpServletResponse response, String id)
+	{
+		Cookie c = new Cookie("SESSION_ID", id);
+		response.addCookie(c);
 	}
 }
