d4ef47035e0c239bd5b21292596d1b108eed007d
siguser <chaoshi@synopsys.com>
2019-11-12 10:42:54 +0800

    fake a log injection

--- src/main/java/com/cc11001100/sqlinject/demo/DemoApplication.java
+++ src/main/java/com/cc11001100/sqlinject/demo/DemoApplication.java
@@ -1,9 +1,13 @@
 package com.cc11001100.sqlinject.demo;
 
+import org.apache.ibatis.javassist.bytecode.stackmap.TypeData;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Cookie;
+import java.util.logging.Logger;
 
 @SpringBootApplication
 
@@ -18,5 +22,11 @@ public class DemoApplication {
 	{
 		Cookie c = new Cookie("SESSION_ID", id);
 		response.addCookie(c);
+	}
+
+	void log_inject(HttpServletRequest req) {
+		String attachment = req.getParameter("attachment");
+		final Logger LOGGER = Logger.getLogger(TypeData.ClassName.class.getName() );
+		LOGGER.info (attachment);
 	}
 }
